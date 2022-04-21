5af24945a0fdb5166d18325a17d08c9434bf4f23
sonoramicoGit <63820466+sonoramicoGit@users.noreply.github.com>
2022-04-21 16:35:53 -0500

    repo java

---
+++ src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+	
+	public static void main(String[] args) {
+		//Pasos para Instalacion
+		
+		System.out.println("1.-Primer paso vamos a inicializar el repositorio"); 
+	}
+
+}
