e3d0d4f598a415a93750888ef6240d98bf13359e
sonoramicoGit <63820466+sonoramicoGit@users.noreply.github.com>
2022-04-21 17:04:20 -0500

    fghfghfgh

--- src/app/App.java
+++ src/app/App.java
@@ -7,7 +7,7 @@ public class App {
 		
 		System.out.println("1.-Primer paso vamos a inicializar el repositorio"); 
 		System.out.println("2.-Segundo paso vamos a cambiar una linea y subirla"); 
-		System.out.println("3.-cinco vamos a cambiar una linea y subirla");
+		System.out.println("3.-seis vamos a cambiar una linea y subirla");
 	}
 
 }
